32626f435aacf3eadb830116db1a3b00f2fd7022
Maxim <vanvanov14@mail.ru>
2026-08-21 15:57:11 +0700

    Final

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +8,29 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+
+    def get_queryset(self):
+        queryset = Stock.objects.all().prefetch_related('positions', 'positions__product')
+        search = self.request.query_params.get('search')
+        products = self.request.query_params.get('products')
+
+        if products:
+            queryset = queryset.filter(positions__product_id=products).distinct()
+
+        if search:
+            queryset = queryset.filter(
+                positions__product__title__icontains=search
+            ) | queryset.filter(
+                positions__product__description__icontains=search
+            )
+            queryset = queryset.distinct()
+
+        return queryset
